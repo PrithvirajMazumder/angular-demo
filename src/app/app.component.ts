de95108b3514abd9d0a0c213dc929dd41f7f2039
PrithvirajMajumder <prithirajmajumder8@gmail.com>
2020-03-12 16:37:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit  {
+  public name: string = '';
+  public title: string = 'my-app';
+
+  private readonly dataStoreKey: string = 'appData';
+
+  public ngOnInit() {
+    const data = this.getData();
+
+    this.name = data.name;
+  }
+
+  public saveData() {
+    let appData: any = {
+      name: this.name
+    };
+    localStorage.setItem(this.dataStoreKey, JSON.stringify(appData));
+  }
+
+  private getData() {
+    const appData = localStorage.getItem(this.dataStoreKey);
+
+    return JSON.parse(appData);
+  }
 }
